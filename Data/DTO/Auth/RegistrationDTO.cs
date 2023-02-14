780b84fc68ccdc5cada5acd998cb8cb786ce83d7
Егор <egor.zholobov@yandex.ru>
2023-02-14 12:32:12 +0300

    Add regex check to password

--- Data/DTO/Auth/RegistrationDTO.cs
+++ Data/DTO/Auth/RegistrationDTO.cs
@@ -6,9 +6,14 @@ namespace Data.DTO.Auth
     public class RegistrationDTO
     {
         [Required]
+        [EmailAddress(ErrorMessage = "Некорректный Email") ]
         public string Email { get; set; }
 
         [Required]
+        [MinLength(8,ErrorMessage = "Пароль должен быть от 8 до 16 символов")]
+        [MaxLength(16,ErrorMessage = "Пароль должен быть от 8 до 16 символов")]
+        [RegularExpression(@"^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9!@#$%^&*]{8,16})$",
+            ErrorMessage = "Пароль должен содержать цифры, строчные и заглавные буквы латинского алфавита, а так же спецсимволы")]
         public string Password { get; set; }
 
         [Required]
