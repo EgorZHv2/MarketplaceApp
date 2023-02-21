1829b878471fb21436f7925306de0bb562e95915
Егор <egor.zholobov@yandex.ru>
2023-02-21 12:30:42 +0300

    Add favorite shops controller

---
+++ WebAPi/Controllers/FavoriteShopsController.cs
@@ -0,0 +1,55 @@
+﻿using Data.DTO;
+using Logic.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteShopsController : BaseController
+    {
+        private IShopService _service;
+        public FavoriteShopsController(IShopService service)
+        {
+            _service = service;
+        }
+        /// <summary>
+        /// Добавить магазин в избранное
+        /// </summary>
+        /// <param name="shopId"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AddShopToFavorites(Guid id)
+        {
+            await _service.AddShopToFavorites(UserId, id);
+            return Ok();
+        }
+        /// <summary>
+        /// Удалить магазин из избранного
+        /// </summary>
+        /// <param name="shopId"></param>
+        /// <returns></returns>
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFavoriteShop(Guid id)
+        {
+            await _service.DeleteShopFromFavorites(UserId, id);
+            return Ok();
+        }
+        /// <summary>
+        /// Показать избранные магазины пользователя
+        /// </summary>
+        /// <param name="filterPaging">Модель пагинации</param>
+        /// <returns>Страница магазинов</returns>
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> ShowUserFavoriteShops([FromQuery] PaginationDTO filterPaging)
+        {
+            var result = await _service.ShowUserFavoriteShops(UserId, filterPaging);
+            return Ok(result);
+        }
+    }
+}
