fc637f9f892191b201bb0e9d133dda8ed84d7256
Dan921 <danil_kupriyanov01@mail.ru>
2023-03-29 21:31:09 +0300

    flex

---
+++ WebAPi/Controllers/FilesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPi.Controllers;
+
+public class FilesController : Controller
+{
+    /// <summary>
+    /// Получить информацию о файле
+    /// </summary>
+    /// <param name="shopId"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetFileInfo(Guid id)
+    {
+        return NotFound();
+    }
+}
